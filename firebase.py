c927a3a92a166ecdc9f1736ce3b77ca34149b6ab
james luo <luojames52@gmail.com>
2018-04-22 03:41:02 -0700

    fix msgApp

--- firebase.py
+++ firebase.py
@@ -7,34 +7,54 @@ from urllib.request import*
 import json
 import requests
 import random
-def getMsg(treads,users,user):
-    print(treads)
+def getMsg(treads,userAry,photoAry):
     r = requests.get(treads, headers = {'User-agent': 'Chrome'})
     theJson = json.loads(r.text)
     treadArray = theJson["data"]["children"]
     curAry = random.choice(treadArray)
     childData = curAry["data"]
-    msg = childData["subreddit_name_prefixed"] + '\n' + childData["title"] + '\n' + childData["url"] + '\n'
-    for lookUp in users:
-        if lookUp["userID"] == user["userID"]:
+    jsonConfig = "https://www.reddit.com" + childData["permalink"] + ".json"
+    userAry.append(jsonConfig)
+    photodata = (childData["preview"]["images"])
+    photoAry.append(photodata[0]["source"]["url"])
+    msg = childData["subreddit_name_prefixed"] + '\n' + childData["title"] + '\n' + "https://www.reddit.com" +childData["permalink"] + '\n'
+    return msg
+def setData(permalinks, userNumber):
+    ref = db.reference("User/" + userNumber +"/permaLinks/" )
+    refData = ref.set(permalinks)
+    ''' for data in refData:
+        if data["userID"] == userNumber:
+            userRef = data
+            print(permalinks)
+            userRef["permaLinks"].set(permalinks)
+   # for link in permalinks:
+    #    i += 1
+'''
+def get_recFeed(curUsr,users,userAry,photoAry):
+    msg = ""
+    for recUser in users:
+        recFeed = None
+        if recUser["userID"] == curUsr["userID"]:
             continue
-        for Foundtread in lookUp["threads"]:
-            if Foundtread == treads:
-                if len(lookUp["threads"]) == 0:
+        for recuserFeed in recUser["threads"]:
+            for userFeed in curUsr["threads"]:
+                if recuserFeed == userFeed:
+                    feedAry = recUser["threads"]
+                    if len(feedAry) == 1 or len(feedAry) == 0:
                         break
-                recommendTread = None
-                while (recommendTread is None and recommendTread == treads):
-                    recommendTread = random.choice(lookUp["threads"])
-                if recommendTread == None:
-                    print(msg)
+                    curFeed = userFeed
+                    while(curFeed == userFeed):
+                        curFeed = random.choice(feedAry)
+                    newFeed = requests.get(curFeed, headers = {'User-agent': 'Chrome'})
+                    newJson = json.loads(newFeed.text)
+                    newTreadArray = newJson["data"]["children"]
+                    curAry = random.choice(newTreadArray)
+                    childData = curAry["data"]
+                    photoAry.append(childData["thumbnail"])
+                    jsonConfig = "https://www.reddit.com" + childData["permalink"] + ".json"
+                    userAry.append(jsonConfig)
+                    msg = "You should checkout " + childData["title"] + " from " + childData["subreddit_name_prefixed"] + '\n' + childData["permalink"] + '\n'
                     return msg
-                newFeed = requests.get(recommendTread, headers = {'User-agent': 'Chrome'})
-                newJson = json.loads(newFeed.text)
-                newTreadArray = newJson["data"]["children"]
-                curAry = random.choice(newTreadArray)
-                childData = curAry["data"]
-                msg += "You should checkout " + childData["title"] + " from " + childData["subreddit_name_prefixed"] + '\n' + childData["url"] + '\n'
-    print (msg)
     return msg
 cred = credentials.Certificate("redtext.json")
 firebase_admin.initialize_app(cred, {
@@ -46,11 +66,15 @@ ref = db.reference("User")
 # Read the data at the posts reference (this is a blocking operation)
 users = ref.get()
 for user in users:
+    userAry = []
+    photoAry = []
     number = user["number"]
     msg = "UserID: " + user["userID"] + '\n'
     for treads in user["threads"]:
-        msg += getMsg(treads,users,user)
+        msg += getMsg(treads,userAry,photoAry)
+    msg += get_recFeed(user,users,userAry,photoAry)
     msg = msg[:-1]
-    #sendMsg(number,msg)
+    setData(userAry,user["userID"])
+    sendMsg(number,msg,photoAry)
     print(msg)
 
--- twillo.py
+++ twillo.py
@@ -1,9 +1,8 @@
-# /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
 from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
-def sendMsg(number,msg):
+def sendMsg(number,msg,photoAry):
     account_sid = "ACba628f6addcff045f5aac1f66150b302"
     auth_token = "47f545a1e86f99e40f35d3d7ca91e576"
 
@@ -13,6 +12,9 @@ def sendMsg(number,msg):
     client.api.account.messages.create(
         to=number,
         from_="+16193040288",
-        body=msg)
+        body=msg,
+
+        media_url = photoAry
+        )
 
 
